171217e75bf7a637159ac2e89d8358735b63400d
Prabhu <vprabhu1771@gmail.com>
2024-12-31 13:02:31 +0530

    Create CategoryController.php
    php artisan make:controller api/v2/CategoryController

---
+++ app/Http/Controllers/api/v2/CategoryController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\api\v2;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $categories = Category::all();
+        
+
+        // Add the full URL path for icon_path to each category
+        foreach ($categories as $category) {
+            $category->icon_path = Category::getIconFullPath($category->icon_path);
+        }
+        
+        return response()->json(['data' => $categories], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Category $category)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Category $category)
+    {
+        //
+    }
+}
