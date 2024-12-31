e012c9d20576977362e70952c4271ffbf532d54d
Prabhu <vprabhu1771@gmail.com>
2024-12-31 12:59:56 +0530

    CategoryResource count added

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -77,4 +77,9 @@ class CategoryResource extends Resource
             'edit' => Pages\EditCategory::route('/{record}/edit'),
         ];
     }
+
+    public static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::count();
+    }
 }
