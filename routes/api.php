1412ca5f78229d4d8d0d2432afffcd8e62a51753
Prabhu <vprabhu1771@gmail.com>
2024-12-31 13:11:31 +0530

    CategoryController added to api

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+
+use App\Http\Controllers\api\v2\CategoryController;
+
+Route::get('/categories', [CategoryController::class, 'index']);
