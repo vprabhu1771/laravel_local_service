50ac7f72c98cd375f717309195d83e0291752f0c
Prabhu <vprabhu1771@gmail.com>
2024-12-31 13:14:15 +0530

    AuthController added to api

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,30 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
 
 
 use App\Http\Controllers\api\v2\CategoryController;
 
 Route::get('/categories', [CategoryController::class, 'index']);
+
+
+use App\Http\Controllers\api\v2\AuthController;
+
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::post('/login', [AuthController::class, 'login']);
+
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    
+    Route::get('/user', [AuthController::class, 'getUser']);
+
+    Route::post('/upload-profile-pic', [AuthController::class, 'upload']);
+
+    Route::get('/logout', [AuthController::class, 'logout']);
+
+});
