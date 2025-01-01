f310889035eb19aabb20aef13e9be1158139324d
Prabhu <vprabhu1771@gmail.com>
2025-01-01 11:53:24 +0530

    NotificationResource added
    php artisan make:filament-resource Notification --generate

---
+++ app/Filament/Resources/NotificationResource.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\NotificationResource\Pages;
+use App\Filament\Resources\NotificationResource\RelationManagers;
+use App\Models\Notification;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class NotificationResource extends Resource
+{
+    protected static ?string $model = Notification::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Select::make('customer_id')
+                    ->preload()
+                    ->searchable()
+                    ->relationship('customer', 'name'),
+                Forms\Components\Select::make('provider_id')
+                    ->preload()
+                    ->searchable()
+                    ->relationship('provider', 'name'),
+                Forms\Components\Select::make('service_id')
+                    ->preload()
+                    ->searchable()
+                    ->relationship('service', 'name'),
+                Forms\Components\Textarea::make('comment')
+                    ->maxLength(255),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('customer.name')                    
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('provider.name')                    
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('service.name')                    
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('comment')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListNotifications::route('/'),
+            'create' => Pages\CreateNotification::route('/create'),
+            'edit' => Pages\EditNotification::route('/{record}/edit'),
+        ];
+    }
+}
