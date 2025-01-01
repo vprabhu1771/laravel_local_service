2dffcb9363ae2ac08bfda0e4371aa9e94c739616
Prabhu <vprabhu1771@gmail.com>
2025-01-01 11:25:12 +0530

    Review model and migration added
    php artisan make:model Review -m

---
+++ app/Models/Review.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Review extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = [
+        'customer_id',
+        'provider_id',
+        'service_id',
+        'rating',
+        'comment',
+        'date',
+    ];
+
+    public function customer()
+    {
+        return $this->belongsTo(User::class, 'customer_id');
+    }
+
+    public function provider()
+    {
+        return $this->belongsTo(User::class, 'provider_id');
+    }
+
+    public function service()
+    {
+        return $this->belongsTo(Service::class, 'service_id');
+    }
+}
---
+++ database/migrations/2025_01_01_055436_create_reviews_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('customer_id')->nullable();
+
+            $table->unsignedBigInteger('provider_id')->nullable();
+
+            $table->unsignedBigInteger('service_id')->nullable();
+
+            $table->integer('rating')->nullable();
+            
+            $table->text('comment')->nullable();
+            
+            $table->date('date')->nullable();
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
